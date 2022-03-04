0fc3d97a95e894729674652b9731c0c1b5f56abf
Woojin Park <woojjinpark@gmail.com>
2022-03-05 00:43:21 +0900

    work done

--- src/difint/difint.h
+++ src/difint/difint.h
@@ -6,16 +6,18 @@
 //#define DIFINT_CONSTS(c, ...) DIFINT_CONST(c); DIFINT_CONSTS(##__VA_ARGS__)
 
 #define DIFINT_VAR(v) di::V v(#v)
+#define DIFINT_VAR_2(v1, v2) DIFINT_VAR(v1); DIFINT_VAR(v2)
+#define DIFINT_VAR_3(v1, ...) DIFINT_VAR(v1); DIFINT_VAR_2(##__VA_ARGS__)
+#define DIFINT_VAR_4(v1, ...) DIFINT_VAR(v1); DIFINT_VAR_3(##__VA_ARGS__)
+#define DIFINT_VAR_5(v1, ...) DIFINT_VAR(v1); DIFINT_VAR_4(##__VA_ARGS__)
+#define DIFINT_VAR_6(v1, ...) DIFINT_VAR(v1); DIFINT_VAR_5(##__VA_ARGS__)
+#define DIFINT_VAR_7(v1, ...) DIFINT_VAR(v1); DIFINT_VAR_6(##__VA_ARGS__)
+#define DIFINT_VAR_8(v1, ...) DIFINT_VAR(v1); DIFINT_VAR_7(##__VA_ARGS__)
+#define DIFINT_VAR_9(v1, ...) DIFINT_VAR(v1); DIFINT_VAR_8(##__VA_ARGS__)
+#define DIFINT_VAR_10(v1, ...) DIFINT_VAR(v1); DIFINT_VAR_9(##__VA_ARGS__)
 
-//#define DIFINT_VAR_2(v1, v2) DIFINT_VAR(v1), DIFINT_VAR(v2)
-//#define DIFINT_VAR_3(v1, ...) DIFINT_VAR(v1) DIFINT_VAR_2(##__VA_ARGS__)
-//#define DIFINT_VAR_4(v1, ...) DIFINT_VAR(v1) DIFINT_VAR_3(##__VA_ARGS__)
-//#define DIFINT_VAR_5(v1, ...) DIFINT_VAR(v1) DIFINT_VAR_4(##__VA_ARGS__)
-//#define DIFINT_VAR_6(v1, ...) DIFINT_VAR(v1) DIFINT_VAR_5(##__VA_ARGS__)
-//#define DIFINT_VAR_7(v1, ...) DIFINT_VAR(v1) DIFINT_VAR_6(##__VA_ARGS__)
-//#define DIFINT_VAR_8(v1, ...) DIFINT_VAR(v1) DIFINT_VAR_7(##__VA_ARGS__)
-//#define DIFINT_VAR_9(v1, ...) DIFINT_VAR(v1) DIFINT_VAR_8(##__VA_ARGS__)
-//#define DIFINT_VAR_10(v1, ...) DIFINT_VAR(v1) DIFINT_VAR_9(##__VA_ARGS__)
-//
-//#define DIFINT_VARS(...) DIFINT_VAR(v); DIFINT_VARS(##__VA_ARGS__)
-
+template <typename... VarArgsIn>
+inline constexpr int CountVarArgs(VarArgsIn... InVars) 
+{ 
+	return sizeof...(InVars); 
+}
--- src/difintTest/TestCasesBasicExpression.h
+++ src/difintTest/TestCasesBasicExpression.h
@@ -23,9 +23,11 @@ namespace basicexpression
 		DIFINT_VAR(x3);
 		DIFINT_VAR(x4);
 		
-
 		auto expexpr = x.e(C1) + x + x2 - x3 * C1 * C2.e(x);
 		std::cout << expexpr.strExpr() << std::endl;
+
+		DIFINT_VAR_2(a,b);
+		std::cout << a.strExpr() << std::endl;
 	}
 
 }
